78fdc42f66ab346562238d02440e097bc04e1f77
ithinkthisisarc <developmentarc773@gmail.com>
2019-07-25 11:36:18 -0400

    Better err handling

--- mk.sh
+++ mk.sh
@@ -1,7 +1,7 @@
 DIR=${PWD##*/}
 
 if [[ -z $1 ]]; then
-	/etc/mk/$DIR
+	/etc/mk/$DIR 2> /dev/null || echo "No entry yet, run \`mk.sh add\`"
 elif [[ $1 == "add" ]]; then
 	vim /etc/mk/$DIR || vi /etc/mk/%DIR
 	chmod +x /etc/mk/$DIR || exit 0
